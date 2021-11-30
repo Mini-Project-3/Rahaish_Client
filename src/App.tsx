e3fe9f3c9de4b525a6b8bee6498f4a11c36a8f33
mitali453 <mitali.varshney_cs19@gla.ac.in>
2021-11-30 13:51:48 +0530

    map added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import React from 'react';
-
+import Map from "./Map"; 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="bg-red-700">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Map address={"pari chauk"} city={"noida"}></Map>
     </div>
   );
 }
