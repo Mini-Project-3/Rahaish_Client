58c81aa9662cd5bae6ce0ae3b64ddd1a339e7feb
mitali453 <mitali.varshney_cs19@gla.ac.in>
2022-02-02 19:56:05 +0530

    added validation in Upload Flat

--- src/Pages/appContainer/UploadFlat.tsx
+++ src/Pages/appContainer/UploadFlat.tsx
@@ -2,13 +2,14 @@ import axios from "axios";
 import { useFormik } from "formik";
 import { FC, memo } from "react";
 import { BASE_URL } from "../../Constants/constants";
+import * as yup from "yup";
 interface Props {
 
 
 }
 const UploadFlat: FC<Props> = (props) => {
 
-    const { handleSubmit, getFieldProps } = useFormik({
+    const { handleSubmit, getFieldProps , errors, isSubmitting , touched } = useFormik({
         initialValues: {
             name: "",
             rent: "",
@@ -24,6 +25,35 @@ const UploadFlat: FC<Props> = (props) => {
             description: "",
             // fileUpload: "",
         },
+        validationSchema: yup.object().shape({
+            name:yup.string().matches(/^[A-Za-z ]*$/, 'Enter Alphabets only')
+            .min(2, 'Too Short!')
+            .max(20, 'Too Long!')
+            .required('This field is required'),
+            rent: yup.string().matches(/^[0-9\.]+$/, 'Enter Numbers only').max(8,"Rent not more than 8 digits").required('This field is required'),
+            address: yup.string()
+            .min(2, 'Too Short!')
+            .max(25, 'Too Long!')
+            .required('This field is required'),
+            city:yup.string().matches(/^[A-Za-z ]*$/, 'Enter Alphabets only')
+            .min(3, 'Not a valid city')
+            .max(10, 'Enter a valid city')
+            .required('This field is required'),
+            contact:yup.string().matches(/^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/,"Enter a valid number")
+            .min(10, 'Enter a valid number')
+            .max(13,"Not a valid number")
+            .required('This field is required'),
+            area: yup.string().matches(/^[0-9\.]+$/, 'Enter Numbers only')
+            .min(2, 'Too Short!')
+            .max(10, 'Too Long!')
+            .required('This field is required'),
+            floor: yup.number().required('This field is required').max(25, "Not Authorized by government"),
+            bedroom:  yup.number().required('This field is required').max(20,"Enter the valid information"),
+            bathroom: yup.number().required('This field is required').max(20,"Enter the valid information"),
+            status: yup.string().matches(/^[A-Za-z ]*$/, 'Enter Alphabets only').required('This field is required').max(15,"Only 15 characters allowed"),
+            furnishing: yup.string().matches(/^[A-Za-z ]*$/, 'Enter Alphabets only').required('This field is required').max(15,"Only 15 characters allowed"),
+            description: yup.string().required('This field is required').max(150,"Not more than 150"),
+		}),
         onSubmit: async (data) => {
             const url = BASE_URL + "/flat-upload";
             await axios.post(url, data);
@@ -44,60 +74,72 @@ const UploadFlat: FC<Props> = (props) => {
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Name of Flat</label>
                             <input id="name" required {...getFieldProps("name")} type="text" className="block w-full px-3 py-2 hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter Flat Name" />
+                            <div className="h-1">{touched.name && <div className=" text-red-500 text-xs">{errors.name}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Rent</label>
                             <input id="rent" required {...getFieldProps("rent")} type="text" className="block w-full px-3 py-2  hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter Rent/month" />
+                            <div className="h-1">{touched.rent && <div className=" text-red-500 text-xs">{errors.rent}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Address</label>
                             <input id="address" required {...getFieldProps("address")} type="text" className="block w-full px-3 py-2 hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter Address" />
+                            <div className="h-1">{touched.address && <div className=" text-red-500 text-xs">{errors.address}</div>}</div>
                         </div>
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >City</label>
                             <input id="city" required {...getFieldProps("city")} type="text" className="block w-full px-3 py-2 hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter City" />
+                            <div className="h-1">{touched.city && <div className=" text-red-500 text-xs">{errors.city}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Contact Number</label>
                             <input id="contact" required {...getFieldProps("contact")} type="text" className="block w-full px-3 py-2  hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter Your Contact number" />
+                            <div className="h-1">{touched.contact && <div className=" text-red-500 text-xs">{errors.contact}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Area of House</label>
                             <input id="area" required {...getFieldProps("area")} type="text" className="block w-full px-3 py-2  hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter area (sqft)" />
+                            <div className="h-1">{touched.area && <div className=" text-red-500 text-xs">{errors.area}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Floor of Flat</label>
                             <input id="floor" required {...getFieldProps("floor")} type="number" className="block w-full px-3 py-2 hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter No. of floor" />
+                            <div className="h-1">{touched.floor && <div className=" text-red-500 text-xs">{errors.floor}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >No. of Bedrooms</label>
                             <input id="bedroom" required {...getFieldProps("bedroom")} type="number" className="block w-full px-3 py-2 hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter No. of bedrooms" />
+                            <div className="h-1">{touched.bedroom && <div className=" text-red-500 text-xs">{errors.bedroom}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >No. of Bathrooms</label>
                             <input id="bathroom" required {...getFieldProps("bathroom")} type="number" className="block w-full px-3 py-2  hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Enter No. of bathrooms" />
+                            <div className="h-1">{touched.bathroom && <div className=" text-red-500 text-xs">{errors.bathroom}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Status</label>
                             <input id="status" required {...getFieldProps("status")} type="text" className="block w-full px-3 py-2  hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Completed  or under construction or furnished" />
+                            <div className="h-1">{touched.status && <div className=" text-red-500 text-xs">{errors.status}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Furnishing</label>
                             <input id="furnishing" required {...getFieldProps("furnishing")} type="text" className="block w-full px-3 py-2 hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="furnished or unfurnished" />
+                            <div className="h-1">{touched.furnishing && <div className=" text-red-500 text-xs">{errors.furnishing}</div>}</div>
                         </div>
 
                         <div>
                             <label className=" text-sm font-bold text-gray-700" >Description</label>
                             <textarea id="description" required {...getFieldProps("description")} className="block w-full px-3 py-2  hover:shadow-md  text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" placeholder="Not more than 50 words..."></textarea>
+                            <div className="h-1">{touched.description && <div className=" text-red-500 text-xs">{errors.description}</div>}</div>
                         </div>
                         {/* <div>
                             <label className="block  text-sm font-bold text-gray-700">
@@ -135,4 +177,3 @@ const UploadFlat: FC<Props> = (props) => {
 UploadFlat.defaultProps = {
 }
 export default memo(UploadFlat);
-export { };
